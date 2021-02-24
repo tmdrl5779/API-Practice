6a7b014fa30ed140497ed39326d6bc5964732f86
tmdrl5779 <tmdrl5779@gmail.com>
2021-02-24 16:16:45 +0900

    [ADD] Init

---
+++ src/test/java/API/APIPractice/ApiPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package API.APIPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
